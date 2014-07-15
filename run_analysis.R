d350861347efcde3a3b31d91ab7e4534fd54d0da
Lilliana Pelaez <lpelaez2@users.noreply.github.com>
2014-07-15 15:20:20 -0500

    Update run_analysis.R
    Complete code

--- run_analysis.R
+++ run_analysis.R
@@ -1,5 +1,27 @@
 
-downloadFiles <- function(){
+# run_analysis.R
+# Prepare a tidy data as average of mean and standard deviation variables
+# by activity and subject based on data taken from:
+#  http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones
+#
+# 1.  Prepare files
+# 1.1  Download source file
+# 1.2  Uncompress file to extract desired
+
+# 2.  Prepare a data set containing wanted columns
+# 2.1  Consolidate data for test and train
+# 2.2  Select column number for mean and std
+# 2.3  Subset data based on prior columns 
+# 2.4  Add activity and subject to subset
+# 2.5  Assign descritive names for columns
+
+# 3.  Calculates mean for prior columns by activity ans subject
+# 4.  Create a file for tidy data
+
+
+# prepareFiles
+# Download the file required and uncompress it to make data available
+prepareFiles <- function(){
     sourceFile <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip?accessType=DOWNLOAD"
     targetFile <- "getdata_projectfiles_UCI_HAR_Dataset.zip"
     download.file(sourceFile, destfile=targetFile, method="auto", mode="wb")
@@ -7,22 +29,17 @@ downloadFiles <- function(){
     
 }
 
+# targetColumns
+# Select column numbers for mean and standard deviation, as required
+targetColumns <- function( features ){
+    colNum1 <- grep ("mean\\(\\)\\-[XYZ]", features)
+    colNum2 <- grep ("std\\(\\)\\-[XYZ]",  features)
 
-readFile <- function ( folderName, fileName ){
-    return(read.table( paste(folderName, "/", fileName, sep = "")))
-}
-
-
-targetColumns <- function( measures ){
-    colNum1 <- grep ("mean\\(\\)\\-[XYZ]", measures)
-    colNum2 <- grep ("std\\(\\)\\-[XYZ]",  measures)
-    
     return( sort( union(colNum1, colNum2) ) )
-    
 }
 
-
-# Changes feature names to be descriptive 
+# descriptiveNames
+# Change feature names to be more descriptive 
 descriptiveNames <- function (measures){
     names <- gsub("-", "_", measures)
     names <- gsub("\\(","", names)
@@ -36,45 +53,100 @@ descriptiveNames <- function (measures){
     names <- gsub("__", "_", names)
 }
 
-summarizedData <- function(){
+# loadData
+# Read indicated data into R and add activity name and subject
+loadData <- function(folder, dataType){    
     
+    # load  data   
+    dataSet <- read.table( paste(folder, "/", dataType, "/", "X_", dataType, ".txt", sep = "" ) )
+    dataY   <- read.table( paste(folder, "/", dataType, "/", "Y_", dataType, ".txt", sep = "" ) )
+    dataS   <- read.table( paste(folder, "/", dataType, "/", "subject_", dataType,".txt", sep = "" ) )
+    
+    # add activity names to dataset
+    activities <- read.table (paste(folder, "/activity_labels.txt", sep=""))
+    dataSet$activity <- merge(dataY, activities, by ="V1", all=FALSE)$V2
+    
+    # adds subject to dataset
+    dataSet$subject <- dataS$V1
+    
+    return(dataSet)
+    
+}
+
+# loadFeatures
+# Read feature data into R
+loadFeatures <- function( folder) {
+    features <- read.table ( paste(folder, "/features.txt", sep=""))
+    
+    return( features )
+}
+
+# setColumnNames
+# Assign descriptive names to data set
+setColumnNames <-function(dataSet, features){
+    measures <- targetColumns(features$V2)
+    
+    nombres <- descriptiveNames ( features[measures,2] ) 
+    colnames(dataSet) <- c(nombres,"activity","subject")
+    return (dataSet)
+}
+
+# issolateMeasure
+# Subset by columns for mean and std 
+issolateMeasures <- function( dataSet, features ){
+    measures <- targetColumns(features$V2)
+    ds <- dataSet[, measures]
+    
+    return( ds)
+}
+
+# consolidateData
+# Merge data for test and train in a dataset
+consolidateData <- function(folder){
+    test  <- loadData(folder, "test")
+    train <- loadData(folder, "train")
+    
+    return ( rbind(test, train) )
+}
+
+# prepareTiny
+# Orchestrate process:
+#   *  Get a dataSet including all data, all columns, activity name and subject
+#   *  Select columns for measures wanted (mean and std)
+#   *  Add columns for activity and subject to prior subset
+#   *  Set descriptive names to columns
+prepareData <- function(){
     mainFolder <- "UCI HAR Dataset"
     
-    # load activity labels
-    activities <- readFile (mainFolder, "activity_labels.txt")
+    allData<- consolidateData(mainFolder)
+    features <- loadFeatures(mainFolder)
     
-    # load features 
-    features <- readFile (mainFolder, "features.txt")
-    measures <- targetColumns(features$V2)
-    colNames <- descriptiveNames ( features[measures,2] )  
-    colNames <- c("activity_name", colNames)
+    dataSet <- issolateMeasures(allData, features)
+    dataSet$activity <- allData$activity
+    dataSet$subject <- allData$subject
     
+    dataSet<-setColumnNames(dataSet, features)
     
-    # load test data
-    testX <- readFile( paste(mainFolder, "/test/", sep=""), "X_test.txt" )
-    testY <- readFile( paste(mainFolder, "/test/", sep=""), "Y_test.txt" )
+    return (dataSet)
+}
+
+# processTiny
+# Calculate average by activity and subject
+processData <- function(dataSet){
+    z<-ncol(dataSet)-2
     
-    # load train data
-    trainX <- readFile( paste(mainFolder, "/train/", sep=""), "X_train.txt" )
-    trainY <- readFile( paste(mainFolder, "/train/", sep=""), "Y_train.txt" )
-    
-    
-    # 
-    testSet <- cbind( merge(testY, activities, by.x="V1", by.y="V1", all=FALSE)$V2, testX[,measures] )
-    colnames(testSet) <- colNames
-    
-    trainSet <- cbind( merge(trainY, activities, by.x="V1", by.y="V1", all=FALSE)$V2, trainX[,measures] )
-    colnames(trainSet) <- colNames
-    
-   return( rbind(data.frame(testSet), data.frame(trainSet)))
-     
+    return (aggregate(dataSet[,1:48],  FUN=mean, by=list(Activity=dataSet$activity, Subject=dataSet$subject )))
+
 }
 
 
-downloadFiles()
-
-tidyData <- summarizedData()
-write.table(tidyData, "tidy.txt")
+# -----------------------------------
+# main
+# -----------------------------------
+prepareFiles()
+dataSet <- prepareData()
+tidyData <- processData(dataSet)
+write.table(tidyData, "tidyData.txt")
 
 
 
